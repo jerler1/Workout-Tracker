0542b2945674c3afde75e23a0e4903bc20e0c47e
Bunks <busk_1@yahoo.com>
2021-02-07 22:26:50 -0500

    added view routes and resolved some issues

--- controllers/workoutsController.js
+++ controllers/workoutsController.js
@@ -1,8 +1,24 @@
 // Making a router instead of importing the app.
 const router = require("express").Router();
 const Workout = require("../models/Workout.js");
+const path = require("path");
 
-// ROUTES
+// VIEW ROUTES
+// =========================================
+router.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/../public/index.html"));
+});
+
+router.get("/stats", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/../public/stats.html"));
+});
+
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/../public/exercise.html"));
+});
+
+// API ROUTES
+// =========================================
 
 router.get("/api/workouts", (req, res) => {
   console.log(req.body);
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
   useNewUrlParser: true,
